70e1c534fc3274ca4422baf6c403f2f9d5960175
Gareth Murphy <garethcmurphy@gmail.com>
2017-12-14 11:26:20 +0100

    Add script to setup directories

---
+++ clone.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/SciCatProject/catanie.git
+git clone https://github.com/SciCatProject/catamel.git
+cp envfiles/* catamel/server/
+cp catamel/server/config.local.js catamel/server/config.local
+
